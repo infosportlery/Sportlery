993d066b4a1934ddd9897196b02aacf7193e76ff
Jesse Berkhof <contact@jesseberkhof.nl>
2017-01-10 13:03:19 +0100

    configured db connection

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=159.253.0.25;dbname=jesseau5_wl',
+    'username' => 'jesseau5',
+    'password' => '302ctc1423',
     'charset' => 'utf8',
 ];
