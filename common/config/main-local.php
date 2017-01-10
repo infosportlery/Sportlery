9a4e071affefa59f29efa397ebbfab62aed0ceab
Jesse Berkhof <contact@jesseberkhof.nl>
2017-01-10 13:32:24 +0100

    changed

---
+++ common/config/main-local.php
@@ -0,0 +1,35 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=vserver175.axc.nl;dbname=folkewi175_sportlery',
+            'username' => 'folkewi175',
+            'password' => 'n734snt6',
+            'charset' => 'utf8',
+        ],
+        'gzdb' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=vserver175.axc.nl;dbname=folkewi175_testn',
+            'username' => 'folkewi175',
+            'password' => 'n734snt6',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@app/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.sportlery.nl',
+                'username' => 'mailtest@sportlery.nl',
+                'password' => 'mailtest',
+                'port' => '587',
+                'encryption' => 'tls',
+            ]
+        ],
+    ],
+];
