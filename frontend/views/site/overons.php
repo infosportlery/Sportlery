f5a17c4ca01488a259b306ec355f82d32731cf12
Benjamin Kalle <ab.kalle@live.nl>
2017-01-30 12:02:03 +0100

    change aboutus page part2, Change Text area, 2 colms for ipad, removed spacer at to small a size

--- frontend/views/site/overons.php
+++ frontend/views/site/overons.php
@@ -24,8 +24,8 @@
 
 <div class="container-fluid">
       <div class="row">
-        <div class="col-md-4 col-md-offset-2 text-right">
         <h1 class=""><?= Yii::t('app', 'Over Sportlery'); ?></h1>
+        <div class="col-md-4 col-sm-4 col-md-offset-2 col-sm-offset-2 text-right">
           <p>Sportlery is een platform waar sporters elkaar kunnen vinden, samen wedstrijden en evenementen organiseren, en heel gemakkelijk een sportlocatie kunnen boeken.</p>
           <p>Het idee voor Sportlery is ontstaan door het gevoel dat we heel graag vaker wilden sporten, maar dat niemand kon of dat er geen mogelijke sportlocatie beschikbaar was.</p>
           <p>Toen dachten we, dit moet makkelijker kunnen. Wij kwamen met Sportlery.</p>
@@ -33,10 +33,9 @@
           <p>Daarnaast faciliteert Sportlery alle sportlocaties bij jou in de buurt. Zowel betaald als publiekelijk en binnenhallen als buitenvelden worden aangeboden.</p>
           <p>Zin om een nieuwe sport te ontdekken? Ga een surfworkshop volgen of een beginnersles Krav Maga, het kan allemaal!</p>
         </div>
-        <div class="col-md-1">
-          <div style="border-left:1px solid #e6e6e6;height:100%"></div>
+        <div class="col-md-1 col-sm-4 vertical-rule">
         </div>
-        <div class="col-md-4 text-center">
+        <div class="col-md-4 col-sm-4 text-center">
           Image
         </div>
       </div>
