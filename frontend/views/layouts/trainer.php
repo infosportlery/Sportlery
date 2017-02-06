314bcbe7ef44fa9f0a356904f0e699b46245f2eb
ShadowprodHD <ab.kalle@live.nl>
2017-02-06 14:49:38 +0100

    Update trainer.php

--- frontend/views/layouts/trainer.php
+++ frontend/views/layouts/trainer.php
@@ -22,7 +22,7 @@ SportclubsAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="vendor/twbs/bootstrap/docs/favicon.ico">
+    <link rel="icon" href="@web/images/favicon.ico">
     <?= Html::csrfMetaTags() ?>
     <title>Sportlery</title>
 
