72071a16f20d62121ed696429e556311db991f1d
ShadowprodHD <ab.kalle@live.nl>
2017-02-06 14:49:00 +0100

    Update default.php

--- frontend/views/layouts/default.php
+++ frontend/views/layouts/default.php
@@ -23,7 +23,7 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="vendor/twbs/bootstrap/docs/favicon.ico">
+    <link rel="icon" href="@web/images/favicon.ico">
     <?= Html::csrfMetaTags() ?>
     <title>Sportlery</title>
 
