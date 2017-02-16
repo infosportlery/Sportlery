ffe205e0ef27df2517dc647f4202aad08bc6ad7b
Benjamin Kalle <ab.kalle@live.nl>
2017-02-16 15:09:52 +0100

    inline small javascript

--- frontend/web/js/modal.js
+++ frontend/web/js/modal.js
@@ -10,19 +10,6 @@ $(function(){
     });
     
 });
-$(function(){
-    $('#editUser').click(function (){
-        $('#modal').modal('show')
-            .find('#modalContent')
-            .load($(this).attr('value'))
-    });
-    $('#editAvatar').click(function (){
-        $('#modal').modal('show')
-            .find('#modalContent')
-            .load($(this).attr('value'))
-    });
-
-});
 $(document).ready(function(){
         $("#stepone").modal('show')
         .find('#stepOneContent')
