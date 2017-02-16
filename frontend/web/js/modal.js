efd7bc7dd2e86d0123ea392f063835e89f2ea05a
Benjamin Kalle <ab.kalle@live.nl>
2017-02-16 14:41:11 +0100

    inline small javascript

--- frontend/web/js/modal.js
+++ frontend/web/js/modal.js
@@ -1,3 +1,15 @@
+
+
+$(function(){
+    $(document).on('click','.language',function(){
+        var lang = $(this).attr('id');
+
+        $.post('index.php?r=site/language', {'lang':lang},function(data){
+            location.reload();
+        });
+    });
+    
+});
 $(function(){
     $('#editUser').click(function (){
         $('#modal').modal('show')
@@ -11,8 +23,49 @@ $(function(){
     });
 
 });
-
- $(document).ready(function(){
+$(document).ready(function(){
         $("#stepone").modal('show')
         .find('#stepOneContent')
+});
+
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-89674276-1', 'auto');
+  ga('send', 'pageview');
+
+
+var locations = [
+      ['Bondi Beach', -33.890542, 151.274856, 4],
+      ['Coogee Beach', -33.923036, 151.259052, 5],
+      ['Cronulla Beach', -34.028249, 151.157507, 3],
+      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+      ['Maroubra Beach', -33.950198, 151.259302, 1]
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(-33.92, 151.25),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
     });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) { 
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
