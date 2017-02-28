48efb9850f3c8aa97e671e0c81615f11e1b204c3
Benjamin Kalle <ab.kalle@live.nl>
2017-02-28 15:40:36 +0100

    Contest page Revisit

--- frontend/views/site/contest.php
+++ frontend/views/site/contest.php
@@ -20,7 +20,7 @@
   	</div>
   	<div class="col-md-4 col-md-offset-1 col-sm-12">
   		<!-- Video Contest -->
-  		<iframe style="width: 100%; height:315px;"  src="https://www.youtube.com/embed/3y4G2FyxZnY" frameborder="0" allowfullscreen></iframe>
+  		<iframe style="width: 100%; height:315px;"  src="https://www.youtube.com/embed/V9e3kf26WJo" frameborder="0" allowfullscreen></iframe>
   </div> 
   	</div>
     
@@ -36,24 +36,19 @@
 			<queue-community widget-height='1000px'></queue-community>		
 		</div>
 		<div class="col-md-4 col-sm-12">
-	      <h3><b><?= Yii::t('app', 'PRIJZEN')?></b></h3>
-	      <p style="text-align: justify; line-height: 1.6;"><?= Yii::t('app', 'Door actief mee te doen zul je niet alleen punten scoren en stijgen op de ranglijst, maar maak je ook kans op fantastische prijzen!') ?></p>
-	      <h4><b><?= Yii::t('app', 'THE LUCKY WINNER')?></b></h4>
-	      <p><b><?= Yii::t('app', 'aan het eind van de strijd zal één van jullie er met de GoPro vandoor gaan. Per 100 punten die je behaald ontvang je een loterij ticket. Oftewel, hoe actiever je bent, hoe hoger de kans!') ?></b></p>
-          <?= html::img('@web/images/hero-contest-img.png', ['class' => 'prize-image']); ?>
-	      <br>
-	      <h4><?= Yii::t('app', 'GEARFIT2') ?></h4>
+	      <h3><b><?= Yii::t('app', 'PRICES')?></b></h3>
+	      <p style="text-align: justify; line-height: 1.6;"><?= Yii::t('app', 'By joining actively in the contest you will make a chance on the fantastic prices. We have two different categories: the rankings and the lottery. In the rankings it is all about becoming number 1 and with the lottery you get lottery tickets by active participation.') ?></p>
+	      <h3><b><?= Yii::t('app', 'THE LOTTERY')?></b></h3>
+	      <p><b><?= Yii::t('app', 'Every two weeks we will give away this cool drone under our participants. For every 100 points that you make you will get a lottery ticket. So the more active you are, the higher your chances to win!') ?></b></p>
+          <?= html::img('@web/images/drone-contest.png', ['class' => 'prize-image']); ?>
+	      <h3><b><?= Yii::t('app', 'THE RANKINGS') ?></b></h3>
+	      <p><b><?= Yii:t('app', 'At the end one of you will go home with that GoPro. Make sure you claim the number one spot on the rankings you see on the left. But there are way more prices for the top 50 participants.') ?></b></p>
+	      <h4><?= Yii::t('app', '#1 THE LUCKY WINNER!') ?></h4>
+	      <?= html::img('@web/images/hero-contest-img.png', ['class' => 'prize-image']); ?>
+	      <h4><?= Yii::t('app', '#2 SAMSUNG GEAR 2 WATCH') ?></h4>
 	      <?= html::img('@web/images/contest-gear-fit2.png', ['class' => 'prize-image']); ?>
-	      <h4><?= Yii::t('app', 'De Kaag Priveles') ?></h4>
-	      <?= html::img('@web/images/brilpingpong.png', ['class' => 'prize-image']); ?>
-	      <h4><?= Yii::t('app', 'Maximum Sports') ?></h4>
-	      <?= html::img('@web/images/maximumsports.png', ['class' => 'prize-image']); ?>
-	      <h4><?= Yii::t('app', 'GolfLes') ?></h4>
-	      <?= html::img('@web/images/golfles.png', ['class' => 'prize-image']); ?>
-	      <h4><?= Yii::t('app', 'Decatlon gift') ?></h4>
-	      <?= html::img('@web/images/logo-giftcard.jpg', ['class' => 'prize-image']); ?>
-	      <h4><?= Yii::t('app', 'brilpingpong') ?></h4>
-	      <?= html::img('@web/images/brilpingpong.png', ['class' => 'prize-image']); ?>
+	      <h4><?= Yii::t('app', '#3 SAILING COURSE AT "DE KAAG"') ?></h4>
+	      <?= html::img('@web/images/de_kaag_img.png', ['class' => 'prize-image']); ?>
 	    </div>
 	</div>
 </div>
@@ -62,28 +57,11 @@
 <hr class="hr-sm">
 	<div class="row">
 		<div class="col-md-6 col-sm-12 text-rankings-col">
-			<h2 class="text-center">
+			<h3 class="text-center">
 				<b>
-					RANKINGS
+					<?= Yii::t('app', 'LOTTERY')?>
 				</b>
-			</h2>
-			<div class="content-rankings">
-				<p class="content-rankings-p">
-					<?= Yii::t('app', 'Here you can compete for a spot in the rankings. You can earn great prices.') ?> 
-				</p>
-				<p class="content-lottey-p">
-					<?= Yii::t('app', 'Prices') ?>
-				</p>
-			</div>
-		</div>
-		<hr class="hr-sm hr-split-rank-lot">
-		<div class="col-md-6 col-sm-12">
-			<h2 class="text-center">
-				<b>
-					LOTTERY
-				</b>
-			</h2>
-
+			</h3>
 			<div class="content-lottery">
 				<p class="content-lottey-p">
 					<?= Yii::t('app', 'Beside the rankings we also “verloten” prices among our active participants.') ?>
@@ -101,6 +79,54 @@
 					<?= Yii::t('app', '12 April - uitreiking drone');?><br>
 				</p>
 			</div>
+		</div>
+		<hr class="hr-sm hr-split-rank-lot">
+		<div class="col-md-6 col-sm-12">
+			<h3 class="text-left">
+				<b>
+					<?= Yii::t('app', 'WIN A DRONE') ?>
+				</b>
+			</h3>
+			<hr class="hr-invisible-sm">
+			<div class="content-lottery">
+	      		<?= html::img('@web/images/drone-contest.png', ['class' => 'prize-image-lower']); ?>
+			</div>
+			
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12 text-center">
+			<h4><?= Yii::t('app', 'RANKINGS') ?></h4>
+			<p class="content-lottey-p"><?= Yii::t('app', 'Here you can compete for a spot in the rankings. You can earn great prices.')?></p>
+		</div>
+	</div>
+	<div class="row text-center">
+		<div class="col-md-12 text-center">
+	      <?= html::img('@web/images/hero-contest-img.png', ['class' => 'price-image-lower']); ?>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 col-sm-12 text-center">
+	      <?= html::img('@web/images/contest-gear-fit2.png', ['class' => ' price-image-lower']); ?>
+		</div>
+		<div class="col-md-6 col-sm-12 text-center">
+			<?= html::img('@web/images/golfles.png', ['class' => 'price-image-lower']); ?>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 col-sm-12 text-center">
+	      <?= html::img('@web/images/de_kaag_img.png', ['class' => 'price-image-lower']); ?>
+		</div>
+		<div class="col-md-6 col-sm-12 text-center">
+	      <?= html::img('@web/images/maximumsports.png', ['class' => 'price-image-lower']); ?>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6 col-sm-12 text-center">
+			<?= html::img('@web/images/logo-giftcard.jpg', ['class' => 'price-image-lower']); ?>
+		</div>
+		<div class="col-md-6 col-sm-12 text-center">
+			<?= html::img('@web/images/brilpingpong.png', ['class' => 'price-image-lower']); ?>
 		</div>
 	</div>
 </div>
