c482812f2783affa768451236bd76aa512a71ea5
ShadowprodHD <ab.kalle@live.nl>
2017-03-04 22:35:44 +0100

    Update index.php

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -12,68 +12,98 @@
 <div class="header">
   <div class="container">
     <div class="row">
-      <div class="col-md-12">
+      <div class="col-md-12 text-center">
         <h1 class="thin">Sportlery</h1>
         <p class="lead thin">TOGETHER WE SPORT</p>
-        <br>
       </div>
     </div>
     <!-- a -->
     <hr class="hr-invisible-sm">
     
-    <div class="row text-center">
-      <h3 style="color: white;"><b><?= Yii::t('app', 'Soon the online contest will begin!'); ?></b></h3>
-      <hr class="hr-invisible-sm">
-      <h3 style="color: white;"><b><?= Yii::t('app', 'Leave your email behind and get your first points!'); ?></b></h3>
-      <div class="mailing col-md-12">
-        <hr class="hr-invisible hr-invisible-subscribe">
-                   <!-- Begin MailChimp Signup Form -->
-        <style type="text/css">
-          #mc_embed_signup{ clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
-          /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-             We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-        </style>
-        <div id="mc_embed_signup">
-        <form action="//sportlery.us15.list-manage.com/subscribe/post?u=aa872f36fd0b3fc238705937c&amp;id=3032f2fb1e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-            <div id="mc_embed_signup_scroll">
-          
-          <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="<?= Yii::t('app', 'Email Address') ?>" required>
-            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_aa872f36fd0b3fc238705937c_3032f2fb1e" tabindex="-1" value=""></div>
-            <div class="clear" style="min-width: 25%; display: inline-block;"><input type="submit" value="<?= Yii::t('app', 'Subscribe') ?>" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-            </div>
-        </form>
+    <div class="row">
+      <div class="col-md-12 col-sm-12">
+        <div class="row">
+          <div class="col-md-6 col-md-offset-3">
+            <iframe style="width: 100%; height:315px;"  src="https://www.youtube.com/embed/BfjCgfLW7Ec" frameborder="0" allowfullscreen></iframe>
+         
+          </div>
+            
         </div>
-
-        <!--End mc_embed_signup-->        
-
+        <div class="row text-center">
+            <h3 style="color: white;"><b><?= Yii::t('app', 'Sportlery is an online sports community where people can find each other to sport together, organise sport events and book sport locations.'); ?></b></h3>  
+            <h3 style="color: white;"><b><?= Yii::t('app', 'SIGNUP TO THE CONTEST BELOW!'); ?></b></h3>
+          
+        </div>
       </div>
-    </div>
+      <div class="col-md-4 col-md-offset-1 col-sm-12">
+        <!-- Video Contest -->
+        
+      </div> 
+   </div>
 
     <!-- /a -->
 
   </div>
   <div class="row" id="sliderone"></div>
 </div>
-
+<div class="sub-header text-center" style="max-width: 100%;">
+  <div class="row index-contest-signup" style="max-width: 100%;">
+    <div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-11 col-xs-offset-1">
+      <h3 style="color: white;"><b><?= Yii::t('app', 'Join now and win a GoPro!'); ?></b></h3>  
+    </div>
+    <div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-11 col-xs-offset-1">
+      <queue-signup type="horizontal"></queue-signup>  
+    </div>
+    
+  </div>
+</div>
 
 <div class="container">
    
+  
+
+
+</div>
+<hr class="hr-invisible-sm" id="">
+
+<div class="container">
+  <queue-greeting-bar></queue-greeting-bar>
+  <queue-exit-popup></queue-exit-popup>
+
+
+  <div class="row">
+    <div class="col-md-6 col-md-offset-1 col-sm-12">
+      <h4><?= Yii::t('app', 'What is our community saying?'); ?></h4>
+      <queue-socialproof></queue-socialproof>
+    </div>
+    <div class="col-md-4 col-sm-12">
+      <h3><b><?= Yii::t('app', 'PRICES')?></b></h3>
+        <p style="text-align: justify; line-height: 1.6;"><?= Yii::t('app', 'By joining actively in the contest you will make a chance on the fantastic prizes. We have two different categories: the rankings and the lottery. In the rankings it is all about becoming number 1 and with the lottery you get lottery tickets by active participation.') ?></p>
+        <h3><b><?= Yii::t('app', 'THE LOTTERY')?></b></h3>
+        <p><b><?= Yii::t('app', 'Every two weeks we will give away this cool drone under our participants. For every 50 points that you make you will get a lottery ticket. So the more active you are, the higher your chances to win!') ?></b></p>
+          <?= html::img('@web/images/drone-contest.png', ['class' => 'prize-image']); ?>
+        <h3><b><?= Yii::t('app', 'THE RANKINGS') ?></b></h3>
+        <p><b><?= Yii::t('app', 'At the end one of you will go home with that GoPro. Make sure you claim the number one spot on the rankings you see on the left. But there are way more prizes for the top 50 participants.') ?></b></p>
+        <?= html::img('@web/images/hero-contest-img.png', ['class' => 'prize-image']); ?>
+    </div>
+  </div>
+
+  <div class="row" id="sliderone"></div>
+</div>
+<hr class="hr-invisible-sm">
+<div class="container" >
   <div class="row" >
     <div class="col-md-4 col-md-offset-4 text-center">
       <h1><?= Yii::t('app', 'What is Sportlery?'); ?></h1>
     </div> <!-- end col -->
   </div> <!-- end row -->
   <div class="row text-center">
-    <div class="col-md-6 col-md-offset-3 col-sm-12"> 
-      <iframe style="width: 100%;" height="315" src="https://www.youtube.com/embed/3y4G2FyxZnY" frameborder="0" allowfullscreen></iframe>
-    </div>
+    <div class="col-md-6 col-md-offset-3 col-sm-12"> <iframe style="width: 100%;" height="315" src="https://www.youtube.com/embed/3y4G2FyxZnY" frameborder="0" allowfullscreen></iframe></div>
+     
   </div>  
-
-
-    <hr class="hr-invisible-sm">
+  <hr class="hr-invisible">
   <div class="row text-center">
-    <div class="col-md-10 col-xs-10 col-sm-10 col-sm-offset-1 col-xs-offset-1 col-md-offset-1">
+    <div class="col-md-10 col-md-offset-1 col-sm-12 col-xs-12">
       <div class="col-md-4 col-sm-4 infomation-col" id="index-slideup-obj">
         <?= Html::img('@web/images/icons/location.png', ['width' => '80']); ?>
         <h1 class="h1-default text-center"><strong><?= Yii::t('app', 'Sport Locations'); ?></strong></h1>
@@ -83,7 +113,8 @@
       </div>
       <div class="col-md-4 col-sm-4 infomation-col" id="index-slideup-obj-2">
         <?= Html::img('@web/images/icons/matchmaking.png', ['width' => '80']); ?>
-        <h1 class="h1-default text-center"><strong><?= Yii::t('app', 'Matchmaking'); ?></strong></h1>
+        <h1 class="h1-default text-center h1-header-sub-1"><strong><?= Yii::t('app', 'Matchmaking'); ?></strong></h1>
+        <h1 class="h1-default text-center h1-header-sub-2"><strong><?= Yii::t('app', 'Match making'); ?></strong></h1>
         <p><?= Yii::t('app', 'Find sportbuddies, create or participate in events and find substitute players for your team.') ?></p>
         <hr class="hr-sm">
          <?= Html::button(Yii::t('app', 'Read more!'), ['class' => 'btn btn-primary information-btn', 'id' => 'btn-whatis-more']); ?>
@@ -93,7 +124,7 @@
         <h1 class="h1-default text-center"><strong><?= Yii::t('app', 'Sport Classes'); ?></strong></h1>
         <p><?= Yii::t('app', 'Discover and try-out new sports! A clear overview of all classes and workshops that are being offered in your neighbourhood.'); ?></p>
         <hr class="hr-sm">
-        <?= Html::button(Yii::t('app', 'Read more!'), ['class' => 'btn btn-primary information-btn', 'id' => 'btn-message-more']); ?>
+        <?= Html::button(Yii::t('app', 'Under Construction'), ['class' => 'btn btn-primary information-btn']); ?>
       </div>
     </div>
   </div> 
@@ -109,10 +140,10 @@
 <div class="container text-center" >
   <div class="row" id="index-mm">
     <div class="row">
-      <div class="col-md-12">
+      <div class="col-md-12 col-sm-12 col-xs-12">
         <h1 style="color: black;">Matchmaking</h1>  
       </div>
-      <div class="col-md-12">
+      <div class="col-md-12 col-sm-12 col-xs-12">
         <?php if(Yii::$app->language == 'nl'){
           echo '<div class="img-responsive img-mm-type" id="img-mm-nl"></div>';
         } else {
@@ -135,10 +166,10 @@
 
   <div class="row" id="index-loc">
     <div class="row">
-      <div class="col-md-12">
+      <div class="col-md-12 col-sm-12 col-xs-12">
         <h1 class="loc-head"><?= Yii::t('app', 'Sport Locations'); ?></h1>  
       </div>
-      <div class="col-md-12">
+      <div class="col-md-12 col-sm-12 col-xs-12">
       <?php if(Yii::$app->language == 'nl'){
           echo '<div class="img-responsive img-loc-type" id="img-loc-nl"></div>';
         } else {
@@ -157,6 +188,7 @@
 
 <!-- Parallax One
 ************************************************************************ -->
+
 
 
 
@@ -183,12 +215,12 @@
 </div> <!-- end parallaxTwo -->
 
 <div class="parallax3" style="background-color: rgb(244, 104, 53);padding: 25px 0 25px 0px;">
-  <div class="container" style="background-color: rgb(244, 104, 53);">
-      <div class="row text-center">
-      <h3 style="color: white;"><b><?= Yii::t('app', 'Do you want to become one of the first Sportlers? Fill in your e-mail address below:'); ?></b></h3>
-      <div class="mailing col-md-12">
-        <div style="text-align: left; font-weight: bold; color: rgb(136, 136, 136); padding: 10px; font-family: Arial;"></div> 
-                   <!-- Begin MailChimp Signup Form -->
+	<div class="container" style="background-color: rgb(244, 104, 53);">
+		  <div class="row text-center">
+	    <h3 style="color: white;"><b><?= Yii::t('app', 'Do you want to become one of the first Sportlers? Fill in your e-mail address below:'); ?></b></h3>
+	    <div class="mailing col-md-12">
+
+        <!-- Begin MailChimp Signup Form -->
         <style type="text/css">
           #mc_embed_signup{ clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
           /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
@@ -206,11 +238,10 @@
         </form>
         </div>
 
-        <!--End mc_embed_signup-->        
-
-      </div>
-    </div>
-  </div>
+        <!--End mc_embed_signup-->  
+	    </div>
+	  </div>
+	</div>
 </div>
 <div class="container">
 
@@ -241,3 +272,5 @@
   <hr class="hr-invisible-sm">
 
 </div>
+
+
