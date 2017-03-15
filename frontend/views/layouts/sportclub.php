81a167fd2397a4918b3c6eff6fd46450d14af55e
ShadowprodHD <ab.kalle@live.nl>
2017-03-15 15:48:27 +0100

    Update sportclub.php

--- frontend/views/layouts/sportclub.php
+++ frontend/views/layouts/sportclub.php
@@ -37,11 +37,14 @@ SportclubsAsset::register($this);
 
     <div class="header3">
           <div class="container">
-            <div class="row text-left">
-              <div class="col-md-6">
+            <div class="row">
+              <div class="col-md-6 col-sm-12 text-left">
                 <h1><?= Yii::t('app','Extend your reach!'); ?></h1>
                 <p class="lead"><?= Yii::t('app', 'Sportlery has large and diverse community. All of them want to practice their favourite sport, but don&apos;t always know where to go. By offering your sport location, you can attract more people and increase your occupancy!'); ?></p>
                 <button class="btn btn-default" id="btn-sportclub"><?= Yii::t('app','Read more!'); ?></button>
+              </div>
+              <div class="col-md-5 col-md-offset-1 col-sm-12" style="margin-top: 50px;">
+                <iframe width="100%" height="315" src="https://www.youtube.com/embed/IVrDoaDp0-o" frameborder="0" allowfullscreen=""></iframe>
               </div>
             </div>
         </div>
