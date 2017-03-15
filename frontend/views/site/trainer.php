47aaa32ad1d2338fc58838c124526ee4e39107de
ShadowprodHD <ab.kalle@live.nl>
2017-03-15 16:29:56 +0100

    Update trainer.php

--- frontend/views/site/trainer.php
+++ frontend/views/site/trainer.php
@@ -59,7 +59,35 @@ use yii\helpers\Html;
   <hr class="hr-invisible">
 
 </div> <!-- end container -->
+<div class="parallax3" style="background-color: rgb(244, 104, 53);padding: 25px 0 25px 0px;">
+	<div class="container" style="background-color: rgb(244, 104, 53);">
+		  <div class="row text-center">
+	    <h3 style="color: white;"><b><?= Yii::t('app', 'Do you want to become one of the first Trainers? Fill in your e-mail address below:'); ?></b></h3>
+	    <div class="mailing col-md-12">
+      
+ <!-- Begin MailChimp Signup Form -->
+<style type="text/css">
+	#mc_embed_signup{ clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="//sportlery.us15.list-manage.com/subscribe/post?u=aa872f36fd0b3fc238705937c&amp;id=4e826d7a1b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_aa872f36fd0b3fc238705937c_4e826d7a1b" tabindex="-1" value=""></div>
+    <div class="clear" style="min-width: 25%; display: inline-block;"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
 
+<!--End mc_embed_signup-->
 
+	    </div>
+	  </div>
+	</div>
+</div>
 
 <hr class="hr-invisible">
