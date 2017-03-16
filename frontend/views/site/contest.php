ea4099d1c74f6764c9c715f3d064b90f87c7ee18
ShadowprodHD <ab.kalle@live.nl>
2017-03-16 14:01:11 +0100

    Update contest.php

--- frontend/views/site/contest.php
+++ frontend/views/site/contest.php
@@ -15,7 +15,7 @@
 	<div class="col-md-5 col-md-offset-1 col-sm-12 text-center">
 		<h2><b>Launch @ 16th of April!</b></h2>
 		<h3 style="line-height:1.5;">Like &amp; Follow us<br/>Share with your friends<br/>Earn points and WIN!</h3>
-		<button class="btn btn-default" id="howto-contest-btn">How to win?</button>
+		<a href="#howtowin-contest" class="btn btn-default" id="howto-contest-btn">How to win?</a>
 	</div>
 	<div class="col-md-5 col-sm-12">
 		<!-- Video Contest -->
@@ -54,7 +54,7 @@
 	</div>
 </div>
 
-<div class="container" id="howto-contest">
+<div class="container" id="howtowin-contest">
 	<hr class="hr-sm">
 	<div class="row">
 		<div class="col-md-12">
