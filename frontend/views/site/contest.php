f0499d2e7683d6ff8c643317005b950bbecbe98e
ShadowprodHD <ab.kalle@live.nl>
2017-04-12 15:32:08 +0200

    Update contest.php

--- frontend/views/site/contest.php
+++ frontend/views/site/contest.php
@@ -14,7 +14,7 @@
 	<h1 style="color: white;" class="text-center"><b><?= Yii::t('app', 'Welcome to the Ultimate Sports Community!'); ?></b></h1>
 	<hr class="hr-sm">
 	  <div class="col-md-5 col-md-offset-1 col-sm-12 text-center">
-		<h2 style="margin-top: 0px;"><b>Launch @ 16th of April!</b></h2>
+		<h2 style="margin-top: 0px;"><b>Contest ends in</b> <b id="demo"></b></h2>
 		  <hr class="hr-invisible-sm">
 		<h3 style="line-height:1.5;">Like &amp; Follow us,<br/>Share with your friends,<br/>Earn points and... WIN!</h3>
 		  <hr class="hr-invisible-sm">
@@ -361,3 +361,34 @@
 		</div>
 	</div>
 </div>
+
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Apr 16, 2017 18:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+</script>
